f31bf4334301ac4bd004efd0b49aded9a2459887
Jorge Figueroa (Qosmio W8) <yknx4@outlook.com>
2014-02-28 20:41:34 -0600

    fix routes

--- Node Server/sensordata.js
+++ Node Server/sensordata.js
@@ -20,6 +20,14 @@ this.getSensorDataFromMsg = function (msg) {
         var sensor_msg = full_msg.substring(1 + vars.msgSize * i, vars.msgSize + vars.msgSize * i);
         sensor_data[i] = sensor_msg;
     }
-    var result = this.newSensorData(new Date(), sensor_data);
+    //    var result = this.newSensorData(new Date(), sensor_data);
+    /*This will be just for randomizing*/
+    var curDate = new Date();
+    var ranMonth = Math.floor((Math.random() * 5));
+    var ranDay = Math.floor((Math.random() * 28) + 1);
+    var ranHour = Math.floor((Math.random() * 23) + 1);
+    var ranMinute = Math.floor((Math.random() * 59) + 1);
+    var fDate = new Date(curDate.getFullYear(), ranMonth, ranDay, ranHour, ranMinute, 0);
+    var result = this.newSensorData(fDate, sensor_data);
     return result;
 }
