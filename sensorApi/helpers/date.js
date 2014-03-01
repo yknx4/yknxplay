07c2d6b4b69a162eeb8b91a87927c92bac50db82
Jorge Figueroa (Qosmio W8) <yknx4@outlook.com>
2014-03-01 11:11:36 -0600

    Mix servers in 1

--- sensorApi/helpers/date.js
+++
@@ -1,6 +0,0 @@
-exports.parseDateInUrl = function (date) {
-    var dateArray = date.split("-");
-    //año,mes,dia,hora,minutos,segundos
-    var finalDate = new Date(dateArray[0], dateArray[1] - 1, dateArray[2], dateArray[3], dateArray[4], dateArray[5]);
-    return finalDate;
-};
