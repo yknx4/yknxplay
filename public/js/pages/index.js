e6128b16fd9b0e7136492c476cdf2249dc4b5251
Jorge Figueroa (Qosmio W8) <yknx4@outlook.com>
2014-03-06 08:59:00 -0600

    graph

--- public/js/pages/index.js
+++ public/js/pages/index.js
@@ -1,9 +1,11 @@
 $(document).ready(function () {
-    $.get("/sensor_data/month/1", {}, function (data) {
-        var fData = JSON.parse(data).sensordata;
+    $.get("/sensor_data/month/2", {}, function (data) {
+        var jData = JSON.parse(data);
+        var fData = jData.sensordata;
         var values = new Array();
+
         for (var i = 0; i < fData.sensorValues.length; i++) {
-            values[i] = (fData.sensorValues[i] / fData.count) * (1 + .33 * i);
+            values[i] = (fData.sensorValues[i] / fData.count) * (1 + .4 * i);
         }
         console.log(fData);
         console.log(JSON.stringify(values));
@@ -26,5 +28,69 @@ $(document).ready(function () {
                 }
             }
         });
+        /*OTHER CHART*/
+        var daysData = fData.days;
+        var fDataC = new Array();
+
+
+        daysData.forEach(function (val, index, arr) {
+            var vDate = index + 1;
+            var avg = (val.sensorValues[0] + val.sensorValues[1] + val.sensorValues[2]) / 3;
+            if (val.count > 0)
+                fDataC.push({
+                    year: JSON.stringify(vDate),
+                    s0: val.sensorValues[0],
+                    s1: val.sensorValues[1],
+                    s2: val.sensorValues[2],
+                    avg: avg
+                });
+        });
+
+        $("#duoChartContainer").dxChart({
+            dataSource: fDataC,
+            commonSeriesSettings: {
+                type: "spline",
+                argumentField: "year"
+            },
+            commonAxisSettings: {
+                grid: {
+                    visible: true
+                }
+            },
+            series: [
+                {
+                    valueField: "s0",
+                    name: "Sensor 1"
+                },
+                {
+                    valueField: "s1",
+                    name: "Sensor 2"
+                },
+                {
+                    valueField: "s2",
+                    name: "Sensor 3"
+                },
+                {
+                    valueField: "avg",
+                    name: "Average",
+                    type: 'splinearea'
+                }
+    ],
+            tooltip: {
+                enabled: true
+            },
+            legend: {
+                verticalAlignment: "bottom",
+                horizontalAlignment: "center"
+            },
+            title: "Flujo de Agua",
+            commonPaneSettings: {
+                border: {
+                    visible: true,
+                    bottom: false
+                }
+            }
+        });
+
     }, "text");
 });
